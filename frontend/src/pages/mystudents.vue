4a2c19d466ee5e51d35d42cbf16a7ea4db848454
newedward <edward777zhu@gmail.com>
2021-01-26 15:31:53 +0800

    我的学生界面

--- frontend/src/pages/mystudents.vue
+++ frontend/src/pages/mystudents.vue
@@ -7,7 +7,29 @@
         <flowTea></flowTea>
       </el-aside>
         <el-main>
+          <el-row type="flex" justify="center">
+            <span class="title">等待队列</span>
+          </el-row>
+          <el-row>
+<draggable  v-model="groups" @chang="change" @start="start" @end="end" :move="move">
+    <el-card v-for="item in groups" :key=index class="stu">
+      <el-avatar shape="circle" :src="item.avater"></el-avatar>
+      <span >{{item.code + "-" + item.name}}</span>
+      <el-button style="float: right" icon="el-icon-close">移除</el-button>
+    </el-card>
+</draggable>
+          </el-row>
+          <el-divider></el-divider>
+          <el-row type="flex" justify="center">
+            <span class="title">我的学生</span>
+          </el-row>
+          <el-row type="flex" justify="center" v-for="rows in branch">
+            <el-card class="student" v-for="cols in rows">
 
+              <el-avatar :size="40" shape="circle" :src="cols.avater"></el-avatar>
+              <span class="intro">{{cols.code + cols.name}}</span>
+        </el-card>
+          </el-row>
         </el-main>
       </el-container>
 
@@ -18,26 +40,78 @@
 <script>
   import topNav from '../components/topNav'
 import flowTea from '../components/flowTea'
+  import draggable from 'vuedraggable'
     export default {
         name: "mystudents",
       components: {
       topNav,
       flowTea,
+        draggable,
     },
       data(){
            return {
-
+             groups: [
+        {avater:"https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png",code:"171110133",name:"张三",intro:"不忘初心"},
+            {avater:"https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png",code:"171110133",name:"陈斌",intro:"不忘初心"},
+            {avater:"https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png",code:"171110133",name:"马化腾",intro:"不忘初心"},
+            {avater:"https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png",code:"171110133",name:"李四",intro:"不忘初心"},
+          {avater:"https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png",code:"171110133",name:"马云",intro:"不忘初心"},
+             {avater:"https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png",code:"171110133",name:"马云",intro:"不忘初心"},
+      ],
+             rownumber:5,
+           list: [
+             {avater:"https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png",code:"171110134",name:"朱振南",intro:"不忘初心"},
+            {avater:"https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png",code:"171110135",name:"朱振南",intro:"不忘初心"},
+            {avater:"https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png",code:"171110133",name:"朱振南",intro:"不忘初心"},
+            {avater:"https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png",code:"171110132",name:"朱振南",intro:"不忘初心"},
+          {avater:"https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png",code:"171110131",name:"朱振南",intro:"不忘初心"},
+             {avater:"https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png",code:"171110130",name:"朱振南",intro:"不忘初心"},
+           ],
+             branch:[],
     }
       },
       mounted() {
-
+          this.getRow();
       },
       methods:{
-
+          change (event) {
+      console.log('change', event)
+    },
+    start (event) {
+      console.log('start', event)
+    },
+    end (event) {
+      console.log('end', event, this.groups)
+    },
+    move (event, origin) {
+      console.log('move', event, origin)
+    },
+        getRow () {
+        let arr = [];
+        let row = Math.ceil(this.list.length / this.rownumber);
+        for (let i = 0; i < row; i++) {
+          arr[i] = [];
+          let modLast = this.list.length % this.rownumber === 0 ? this.rownumber : this.list.length % this.rownumber;
+           let lastRow = i === (row - 1) ? modLast : this.rownumber;
+          for (let j = 0; j < lastRow; j++) {
+                 arr[i][j] = this.list[this.rownumber * i + j];
+          }
+         }
+        this.branch = arr;
+       }
       }
     }
 </script>
 
 <style scoped>
-
+.title{
+    font-weight:bold;
+	color: #57acff;
+  }
+  .stu{
+    margin: 10px;
+  }
+  .student{
+    margin: 6px;
+  }
 </style>
