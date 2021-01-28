53cf033c455defced89f373878afe3b8feac2dac
newedward <edward777zhu@gmail.com>
2021-01-28 18:34:24 +0800

    数据库配置

--- grapro/settings.py
+++ grapro/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'backend',
 ]
 
 MIDDLEWARE = [
@@ -73,11 +74,19 @@ WSGI_APPLICATION = 'grapro.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
+# zheng123)
+# eWdy7;hC:q2w
 DATABASES = {
+
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'graDB',
+        'USER': 'root',
+        'PASSWORD': 'eWdy7;hC:q2w',
+        'HOST': '114.55.252.104',
+        'POST': 3306
+
     }
 }
 
